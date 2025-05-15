0989f5569ea1e7a3b104e926330eac22c2e10c64
peterw18 <peterjw2005@gmail.com>
2025-05-15 22:07:34 +0100

    add randomness

--- sl.c
+++ sl.c
@@ -6,37 +6,16 @@
  *        Last Modified: 2014/06/03
  *========================================
  */
-/* sl version 5.03 : Fix some more compiler warnings.                        */
-/*                                              by Ryan Jacobs    2015/01/19 */
-/* sl version 5.02 : Fix compiler warnings.                                  */
-/*                                              by Jeff Schwab    2014/06/03 */
-/* sl version 5.01 : removed cursor and handling of IO                       */
-/*                                              by Chris Seymour  2014/01/03 */
-/* sl version 5.00 : add -c option                                           */
-/*                                              by Toyoda Masashi 2013/05/05 */
-/* sl version 4.00 : add C51, usleep(40000)                                  */
-/*                                              by Toyoda Masashi 2002/12/31 */
-/* sl version 3.03 : add usleep(20000)                                       */
-/*                                              by Toyoda Masashi 1998/07/22 */
-/* sl version 3.02 : D51 flies! Change options.                              */
-/*                                              by Toyoda Masashi 1993/01/19 */
-/* sl version 3.01 : Wheel turns smoother                                    */
-/*                                              by Toyoda Masashi 1992/12/25 */
-/* sl version 3.00 : Add d(D51) option                                       */
-/*                                              by Toyoda Masashi 1992/12/24 */
-/* sl version 2.02 : Bug fixed.(dust remains in screen)                      */
-/*                                              by Toyoda Masashi 1992/12/17 */
-/* sl version 2.01 : Smoke run and disappear.                                */
-/*                   Change '-a' to accident option.                         */
-/*                                              by Toyoda Masashi 1992/12/16 */
-/* sl version 2.00 : Add a(all),l(long),F(Fly!) options.                     */
-/*                                              by Toyoda Masashi 1992/12/15 */
-/* sl version 1.02 : Add turning wheel.                                      */
-/*                                              by Toyoda Masashi 1992/12/14 */
-/* sl version 1.01 : Add more complex smoke.                                 */
-/*                                              by Toyoda Masashi 1992/12/14 */
-/* sl version 1.00 : SL runs vomiting out smoke.                             */
-/*                                              by Toyoda Masashi 1992/12/11 */
+
+/*===========================================
+ *    sl.c: SL-Random version 1.10
+ *        Copyright 2025
+ *                  Peter Walker
+ *                  (peterjw2005@gmail.com)
+ *        Last Modified: 2025/05/15
+ *===========================================
+ */
+ /* SL-Random 1.10: Include random number of carriages */
 
 #include <curses.h>
 #include <signal.h>
@@ -50,6 +29,8 @@ int add_D51(int x);
 int add_sl(int x);
 void option(char *str);
 int my_mvaddstr(int y, int x, char *str);
+int coalLength = 29; // defines the length of a carriage
+int maxCarriages = 20; // maximum number of carriages
 
 int ACCIDENT  = 0;
 int LOGO      = 0;
@@ -97,6 +78,11 @@ int main(int argc, char *argv[])
     leaveok(stdscr, TRUE);
     scrollok(stdscr, FALSE);
 
+    srand(time(0));
+    int n = rand() % maxCarriages;
+    int totalLength = 83 + ( n * coalLength);
+    // 83 for the locomotive, and the rest for the carriages
+
     for (x = COLS - 1; ; --x) {
         if (LOGO == 1) {
             if (add_sl(x) == ERR) break;
@@ -105,11 +91,17 @@ int main(int argc, char *argv[])
             if (add_C51(x) == ERR) break;
         }
         else {
+            // default carriage
             if (add_D51(x) == ERR) break;
         }
         getch();
         refresh();
         usleep(40000);
+
+        
+        if (x < -totalLength){
+            break; // exit when train is no longer visible
+        }
     }
     mvcur(0, COLS - 1, LINES - 1, 0);
     endwin();
@@ -190,6 +182,10 @@ int add_D51(int x)
     for (i = 0; i <= D51HEIGHT; ++i) {
         my_mvaddstr(y + i, x, d51[(D51LENGTH + x) % D51PATTERNS][i]);
         my_mvaddstr(y + i + dy, x + 53, coal[i]);
+        for (j = 1; j <= n; j++){
+            int xcoord = x + 53 + (j * coalLength);
+            my_mvaddstr(y + i + dy, xcoord, coal[i]);
+        }
     }
     if (ACCIDENT == 1) {
         add_man(y + 2, x + 43);
